0e481521ca45d1089a8867e87d7de733cb6450aa
Dhiraj Khapangi <dkhapangi13@outlook.com>
2019-10-29 00:09:53 +0545

    Add Scream Component

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -3,7 +3,8 @@ import {
   LIKE_SCREAM,
   UNLIKE_SCREAM,
   LOADING_DATA,
-  DELETE_SCREAM
+  DELETE_SCREAM,
+  POST_SCREAM
 } from '../types';
 
 const initialState = {
@@ -40,6 +41,11 @@ export default function(state = initialState, action) {
         screams: state.screams.filter(
           scream => scream.screamId !== action.payload
         )
+      };
+    case POST_SCREAM:
+      return {
+        ...state,
+        screams: [action.payload, ...state.screams]
       };
     default:
       return state;
