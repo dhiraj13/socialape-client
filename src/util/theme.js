b5250aa3ae0feac35878081e5c72c451bf1fe619
Dhiraj Khapangi <dkhapangi13@outlook.com>
2019-10-30 10:00:04 +0545

    Displaying Comments

--- src/util/theme.js
+++ src/util/theme.js
@@ -45,6 +45,15 @@ export default {
     },
     link: {
       color: '#00bcd4'
+    },
+    invisibleSeparator: {
+      border: 'none',
+      margin: 4
+    },
+    visibleSeparator: {
+      width: '100%',
+      borderBottom: '1px solid rgba(0,0,0,0.1)',
+      marginBottom: 20
     }
   }
 };
